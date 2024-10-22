072a03422114a5085af585c5f2b4ee14b00d2dc6
patrickw24 <patrick.warner24@gmail.com>
2024-10-21 21:16:43 -0500

    1st commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const incomeModal= ()=>{
+    let incomeM= new bootstrap.Modal(document.getElementById("incomeModal"))
+
+    incomeM.show()
+}
+
+const expenseModal= ()=>{
+
+    let expenseM= new bootstrap.Modal(document.getElementById("expenseModal"))
+
+    expenseM.show()
+}
+
+
+const showIncome= ()=>{
+
+    let type = transType.value
+
+    if(type === "Income"){
+        divAmount.style.display= "block"
+        divIncome.style.display= "block"
+        divExpenses.style.display= "none"
+    }else if(type === "Expenses"){
+        divAmount.style.display= "block"
+        divExpenses.style.display= "block"
+        divIncome.style.display= "none"
+    }
+
+    if(type === ""){
+        divExpenses.style.display= "none"
+        divIncome.style.display= "none"
+    divAmount.style.display= "none"
+    }
+
+}
